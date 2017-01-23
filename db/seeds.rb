18bb1e879581e95e2a483be925c284ac588061e4
johnson-rl <johnson.rl@gmail.com>
2017-01-23 15:44:43 -0800

    seed data created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+p "Hello from seeds.rb"
+
+Speaker.delete_all
+
+p "All those old losers deleted"
+
+excellent_speakers = [
+  {:first=>"Cory", :last=>"Fauver", :email=>"cory@example.com"},
+  {:first=>"Brianna", :last=>"Veenstra", :email=>"brianna@example.com"},
+  {:first=>"Matt", :last=>"Laguardia", :email=>"matt@example.com"},
+  {:first=>"Ilias", :last=>"Tsangaris", :email=>"ilias@example.com"},
+  {:first=>"Zach", :last=>"Cooz", :email=>"DaCoozer@cooz.com"},
+  {:first=>"Shivers", :last=>"McGivers", :email=>"ShivyMcGivey@shivers.com"},
+  {:first=>FFaker::Name.first_name, :last=>FFaker::Name.last_name, :email=>FFaker::Internet.email},
+  {:first=>FFaker::Name.first_name, :last=>FFaker::Name.last_name, :email=>FFaker::Internet.email},
+  {:first=>FFaker::Name.first_name, :last=>FFaker::Name.last_name, :email=>FFaker::Internet.email},
+  {:first=>FFaker::Name.first_name, :last=>FFaker::Name.last_name, :email=>FFaker::Internet.email}
+]
+
+Speaker.create(excellent_speakers)
+
+p "All those new winners added."
